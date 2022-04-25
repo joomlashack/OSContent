056a9632e572c52787e441a6e915056dd6000d16
Bill Tomczak <bill@joomlashack.com>
2022-04-25 15:16:40 -0700

    Use new framework helper

--- src/admin/include.php
+++ src/admin/include.php
@@ -48,17 +48,10 @@ try {
 
         AutoLoader::registerCamelBase('Oscontent', OSCONTENT_LIBRARY . '/joomla');
 
-        if (Version::MAJOR_VERSION < 4) {
-            HTMLHelper::_(
-                'stylesheet',
-                'lib_allediaframework/fontawesome/css/all.min.css',
-                ['relative' => true]
-            );
-        }
-
         Factory::getLanguage()->load('com_oscontent.sys', OSCONTENT_ADMIN);
 
         HTMLHelper::_('jquery.framework');
+        HTMLHelper::_('alledia.fontawesome');
         JLoader::register('OscontentHelper', OSCONTENT_ADMIN . '/helpers/oscontent.php');
 
         define('OSCONTENT_LOADED', 1);
